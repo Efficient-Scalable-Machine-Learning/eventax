8e9080645a26c41377752fccb03c4d8bca5a918a
LuggiStruggi <luggistruggi@gmail.com>
2026-03-05 11:51:39 +0100

    workflow in .github

--- setup.py
+++ setup.py
@@ -1,33 +1,27 @@
 import io
 import os
 from setuptools import setup, find_packages
-
 here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
-
 setup(
     name="eventax",
     version="0.1.0",
     author="Lukas König",
     author_email="lukmkoenig@gmail.com",
     description=(
-        "A Diffrax-based toolkit for continuous-time spiking neural networks "
+        "A Diffrax-based framework for continuous-time spiking neural networks "
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/LuggiStruggi/EventPropJax",
+    url="https://github.com/Efficient-Scalable-Machine-Learning/eventax",
     license="MIT",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
-        "jax>=0.4.0",
-        "diffrax>=0.6.0",
+        "jax>=0.9.1",
+        "diffrax>=0.7.2",
+        "equinox>=0.13.5",
     ],
-    python_requires=">=3.7",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    python_requires=">=3.11",
 )
